4240f7acae2cb23f618dc63a26df7981e37e1170
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-09-01 18:55:20 +0530

    Updated folder structure

--- easyheals/src/components/Partners.jsx
+++ easyheals/src/components/Partners.jsx
@@ -10,35 +10,37 @@ export const Partners = () => {
         <img src="/PlusIcon.svg" alt="Plus Icon" />
         <span className="border-b border-lineColor text-lineColor opacity-30 w-[20px] md:w-[220px] lg:w-[320px] xl:w-[425px] ml-2"></span>
       </div>
-
+    
+    {/* Large Screen Devices */}
       <div className="hidden lg:flex flex-row lg:gap-x-14 xl:gap-x-20 mx-auto mt-10">
-        <img src="/RubyHallClinic.png" className="lg:w-44 lg:h-12 xl:w-52 xl:h-14" alt="Ruby Hall Clinic" />
-        <img src="/DrAgarwals.png" className="lg:w-44 lg:h-12 xl:w-52 xl:h-16" alt="Dr. Agarwals" />
-        <img src="/UnityHospital.png" className="lg:w-24 lg:h-12 xl:w-32 xl:h-14" alt="Unity Hospital" />
-        <img src="/Orion.png" className="lg:w-24 lg:h-12 xl:w-32 xl:h-14" alt="Orion" />
-        <img src="/Sahyadri.png" className="lg:w-24 lg:h-12 xl:w-32 xl:h-14" alt="Sahyadri" />
+        <img src="partners/RubyHallClinic.png" className="lg:w-44 lg:h-12 xl:w-52 xl:h-14" alt="Ruby Hall Clinic" />
+        <img src="partners/DrAgarwals.png" className="lg:w-44 lg:h-12 xl:w-52 xl:h-16" alt="Dr. Agarwals" />
+        <img src="partners/UnityHospital.png" className="lg:w-24 lg:h-12 xl:w-32 xl:h-14" alt="Unity Hospital" />
+        <img src="partners/Orion.png" className="lg:w-24 lg:h-12 xl:w-32 xl:h-14" alt="Orion" />
+        <img src="partners/Sahyadri.png" className="lg:w-24 lg:h-12 xl:w-32 xl:h-14" alt="Sahyadri" />
       </div>
 
-
+    {/* Medium Screen Devices */}
       <div className="hidden lg:hidden md:block flex-row gap-x-20 mx-auto mt-10 flex-wrap">
         <div className="flex flex-row mx-auto gap-x-20">
-          <img src="/RubyHallClinic.png" className="w-42 h-12" alt="Ruby Hall Clinic" />
-          <img src="/DrAgarwals.png" className="w-42 h-12" alt="Dr. Agarwals" />
-          <img src="/UnityHospital.png" className="w-32 h-14" alt="Unity Hospital" />
+          <img src="partners/RubyHallClinic.png" className="w-42 h-12" alt="Ruby Hall Clinic" />
+          <img src="partners/DrAgarwals.png" className="w-42 h-12" alt="Dr. Agarwals" />
+          <img src="partners/UnityHospital.png" className="w-32 h-14" alt="Unity Hospital" />
         </div>
         <div className="flex flex-row mx-auto justify-center gap-x-20 mt-10">
-          <img src="/Orion.png" className="w-24 h-12" alt="Orion" />
-          <img src="/Sahyadri.png" className="w-24 h-12" alt="Sahyadri" />
+          <img src="partners/Orion.png" className="w-24 h-12" alt="Orion" />
+          <img src="partners/Sahyadri.png" className="w-24 h-12" alt="Sahyadri" />
         </div>
       </div>
 
+    {/* Small Screen Devices */}
       <div className="md:hidden flex flex-col items-center justify-center gap-y-20 mx-auto mt-10 mr-32">
-  <img src="/RubyHallClinic.png" className="w-40 h-12" alt="Ruby Hall Clinic" />
-  <img src="/DrAgarwals.png" className="w-40 h-14" alt="Dr. Agarwals" />
-  <img src="/UnityHospital.png" className="w-26 h-14" alt="Unity Hospital" />
-  <img src="/Orion.png" className="w-32 h-14" alt="Orion" />
-  <img src="/Sahyadri.png" className="w-32 h-22" alt="Sahyadri" />
-</div>
+          <img src="partners/RubyHallClinic.png" className="w-40 h-12" alt="Ruby Hall Clinic" />
+          <img src="partners/DrAgarwals.png" className="w-40 h-14" alt="Dr. Agarwals" />
+          <img src="partners/UnityHospital.png" className="w-26 h-14" alt="Unity Hospital" />
+          <img src="partners/Orion.png" className="w-32 h-14" alt="Orion" />
+          <img src="partners/Sahyadri.png" className="w-32 h-22" alt="Sahyadri" />
+      </div>
 
     </div>
   );
