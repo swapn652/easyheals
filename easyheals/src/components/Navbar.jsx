274c4b86c06ee56318e05b0bbd44770c96d09f73
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-09-01 15:25:34 +0530

    Updated Code quality and made responsive for medium screen devices

--- easyheals/src/components/Navbar.jsx
+++ easyheals/src/components/Navbar.jsx
@@ -1,50 +1,29 @@
-import React from 'react'
+import React from 'react';
+
+const commonLinkClasses = `
+   cursor-pointer 
+   hover:underline 
+   hover:decoration-underlineColor
+   hover:decoration-2 
+   hover:underline-offset-8
+`;
+
+const navigationLinks = ["Home", "About Us", "Lab Test", "Treatment"];
 
 export const Navbar = () => {
   return (
-    <nav className="flex flex-row justify-between items-center" style={{fontFamily: 'Poppins'}}>
-        <img src="/easyHealsLogo.png" className="w-44"/>
-        <div className="flex flex-row gap-x-10 items-center">
-            <a className="
-                cursor-pointer 
-                hover:underline 
-                hover:decoration-underlineColor
-                hover:decoration-2 
-                hover:underline-offset-8
-                text-lg">
-                    Home
-            </a>
-            <a className="
-                cursor-pointer 
-                hover:underline 
-                hover:decoration-underlineColor
-                hover:decoration-2 
-                hover:underline-offset-8
-                text-lg">
-                    About Us
-            </a>
-            <a className="
-                cursor-pointer 
-                hover:underline 
-                hover:decoration-underlineColor
-                hover:decoration-2 
-                hover:underline-offset-8
-                text-lg">
-                    Lab Test
-            </a>
-            <a className="
-                cursor-pointer 
-                hover:underline 
-                hover:decoration-underlineColor
-                hover:decoration-2 
-                hover:underline-offset-8
-                text-lg">
-                    Treatment
-            </a>
-            <button className="w-32 h-10 bg-buttonColor text-white rounded-md text-lg font-extrabold px-2">
-                Contact Us
-            </button>
-        </div>
+    <nav className="flex flex-row justify-between items-center" style={{ fontFamily: 'Poppins' }}>
+      <img src="/easyHealsLogo.png" className="md:w-32 lg:w-44" alt="Logo" />   
+      <div className="flex flex-row md:gap-x-6 lg:gap-x-10 items-center md:text-base lg:text-lg">
+        <ul className="flex flex-row md:gap-x-6 lg:gap-x-10">
+          {navigationLinks.map((linkText, index) => (
+            <li key={index} className={commonLinkClasses}>{linkText}</li>
+          ))}
+        </ul>
+        <button className="w-32 h-10 bg-buttonColor text-white rounded-md font-extrabold px-2">
+          Contact Us
+        </button>
+      </div>
     </nav>
-  )
-}
+  );
+};
