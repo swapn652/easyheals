6d9ee16d7f3ac0a0df1d8e2dec2a5623d0da1950
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-09-01 22:11:13 +0530

    Added DoctorsWithFeatures.jsx

---
+++ easyheals/src/components/Doctor/Doctor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Doctor = () => {
+  return (
+    <div className="flex flex-row">
+        <img src="plus_3.png" className="w-32 h-32"/>
+        <img src="femdoctor.png" className="w-80 h-80 mt-4 -ml-40 z-10"/>
+        <img src="plus_1.png" className="w-40 h-40 mt-36 -ml-24"/>
+    </div>
+  )
+}
